2787fbb0ed774baa1755cd83ddfc3140b5ca2e3f
ihages <hagemaij@mail.uc.edu>
2025-02-17 17:04:47 -0500

    constructor updates

--- Wheel.cpp
+++ Wheel.cpp
@@ -4,14 +4,19 @@
 int Wheel::getWheel(){ return ballVal; }
 
 //Setters
-//void setWheel(int inVal){ ballVal = inVal; }
+void Wheel::setWheel(int inVal){ ballVal = inVal; }
 
 //Constructors
-Wheel::Wheel() {
+Wheel::Wheel() { //default constructor
     ballVal = (std::rand() % 10) + 1;
 }
 
-//Functions
-int Wheel::Spin(int min,int max) {
-    ballVal = (std::rand() % max) + 1;
+Wheel::Wheel(int min, int max) { //modified constructor
+    ballVal = (std::rand() % max) + min;
 }
+
+//Functions 
+
+//int Wheel::Spin(int min,int max) { //do we need this if we have the modified constructor?
+//    ballVal = (std::rand() % max) + 1;
+//}
